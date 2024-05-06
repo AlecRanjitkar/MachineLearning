89ec1db93386ec222a76f366248c248bd5019e63
Jawsen <tbs7bjawahir@gmail.com>
2024-05-06 17:08:40 +0200

    "Update to binary matrix"

--- Exam scripts/Exam16Spring/Q11_binary-matrix.py
+++ Exam scripts/Exam16Spring/Q11_binary-matrix.py
@@ -1,49 +1,50 @@
-def find_itemsets_with_support(table, num_items, min_support_ratio):
+import itertools
+
+
+def find_extended_itemsets_with_support(table, num_items, min_support_ratio):
     """
-    Find all itemsets (single items and pairs) that occur with at least a given support ratio.
+    Find all itemsets (single items, pairs, triples, etc.) that occur with at least a given support ratio.
 
     :param table: List of transactions, where each transaction is represented by a list of item flags (0 or 1)
-    :param num_items: The number of items M
+    :param num_items: The number of items (up to 10 in this case)
     :param min_support_ratio: The minimum support ratio
     :return: A list of itemsets that meet the minimum support ratio
     """
     itemsets = []
     num_transactions = len(table)
     min_support_count = min_support_ratio * num_transactions
-    
-    # Check support for individual items
-    for item in range(num_items):
-        support_count = sum(transaction[item] for transaction in table)
-        if support_count >= min_support_count:
-            itemsets.append({f'f{item+1}'})
-    
-    # Check support for pairs of items
-    for item1 in range(num_items):
-        for item2 in range(item1+1, num_items):
-            support_count = sum(transaction[item1] and transaction[item2] for transaction in table)
+
+    # Generate all possible itemsets of size 1 to min(num_items, 10)
+    for size in range(1, min(num_items, 10) + 1):
+        for itemset in itertools.combinations(range(num_items), size):
+            support_count = sum(all(transaction[i] for i in itemset) for transaction in table)
             if support_count >= min_support_count:
-                itemsets.append({f'f{item1+1}', f'f{item2+1}'})
-    
+                itemsets.append({f'x{i+1}' for i in itemset})
+
     return itemsets
 
-# M = 5 items, with support greater than 0.32
-#TODO: Update of M, suport_radio and table based on the examset
-M = 5
-support_ratio = 0.32
-table_4_data = [
-    [0, 1, 1, 0, 1],  # o1
-    [0, 0, 1, 0, 0],  # o2
-    [1, 0, 0, 0, 1],  # o3
-    [1, 0, 0, 1, 1],  # o4
-    [1, 0, 0, 1, 0],  # o5
-    [1, 1, 0, 1, 1],  # o6
-    [1, 0, 1, 1, 0],  # o7
-    [1, 0, 1, 1, 1],  # o8
-    [0, 1, 1, 1, 1],  # o9
-    [1, 0, 1, 0, 0],  # o10
-    [0, 1, 1, 0, 0]   # o11
+# Sample table of transactions
+table_data = [
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],  # Transaction 1
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Transaction 2
+    [1, 1, 0, 1, 0, 0, 1, 1, 0, 0],  # Transaction 3
+    [0, 1, 1, 0, 1, 0, 0, 0, 1, 0],  # Transaction 4
+    [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],  # Transaction 5
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Transaction 6
+    [1, 1, 0, 1, 0, 0, 1, 0, 0, 0],  # Transaction 7
+    [0, 1, 1, 0, 1, 0, 0, 0, 1, 0],  # Transaction 8
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],  # Transaction 9
+    [0, 1, 1, 0, 1, 0, 0, 0, 1, 0],  # Transaction 10
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # Transaction 11
+    [1, 1, 0, 1, 0, 0, 1, 0, 0, 0],  # Transaction 12
+    [0, 1, 1, 0, 1, 0, 0, 0, 1, 0],  # Transaction 13
+    [0, 1, 1, 0, 1, 0, 0, 0, 1, 0]   # Transaction 14
 ]
 
-# Calculate itemsets with support greater than 0.32, including item pairs
-itemsets_with_support = find_itemsets_with_support(table_4_data, M, support_ratio)
-print(itemsets_with_support)
+# Set the number of items and the minimum support ratio
+num_items = 10  # Number of items
+min_support_ratio = 0.40  # Minimum support ratio
+
+# Calculate extended itemsets with support greater than the given ratio
+extended_itemsets_with_support = find_extended_itemsets_with_support(table_data, num_items, min_support_ratio)
+print("Extended itemsets with sufficient support:", extended_itemsets_with_support)
